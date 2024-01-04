30d2aa9c0295224d1e586a3b35ff554d5a52c9f1
Johan Schwind <johanschwind@gmail.com>
2024-01-03 19:49:31 -0500

    Update serial_interface

--- Arduino/serial_interface/serial_interface.ino
+++ Arduino/serial_interface/serial_interface.ino
@@ -1,7 +1,9 @@
 #include <ArduinoJson.h>
+#include <String.h>
 
-StaticJsonDocument<20> outgoing;
-StaticJsonDocument<20> incoming;
+StaticJsonDocument<100> outgoing;
+StaticJsonDocument<900> incoming;
+
 
 void setup() {
   Serial.begin(9600);
@@ -10,18 +12,20 @@ void setup() {
 
 void loop() { 
   if (Serial.available() > 0) {
-
-    String jsonData = Serial.readStringUntil('\n');    
-    DeserializationError error = deserializeJson(incoming, jsonData);
     
-    if (error) {
-      Serial.print("deserializeJson() failed: ");
-      Serial.println(error.c_str());
-
-    } else {   
-    outgoing["msg"] = "ACK";
-     serializeJson(outgoing, Serial);
-    Serial.print('\n');
-    }  
+      DeserializationError error = deserializeJson(incoming, Serial);
+      
+      if (error) {
+        char error_msg[50];
+        strcpy(error_msg, "Arduino Error: ");
+        strcat(error_msg, error.c_str());
+        outgoing["msg"] = error_msg;
+  
+      } else {   
+      outgoing["msg"] = "ACK";       
+      }
+        
+      serializeJson(outgoing, Serial);
+      Serial.print('\n');
   }
 }
